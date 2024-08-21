f960afd5b45d568a3adce1067608e63fef408f7e
mmurta <maxence.murta@itsf.io>
2024-08-21 16:45:56 +0200

    initial

---
+++ src/main/java/com/framework/galaxion/GalaxionApplication.java
@@ -0,0 +1,13 @@
+package com.framework.galaxion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalaxionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalaxionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/framework/galaxion/GalaxionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.framework.galaxion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalaxionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
