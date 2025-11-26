c9b9b81979abc9d87166c8f6e2f27058aa998a94
hfWalid <walidhefhaf@gmail.com>
2025-11-26 14:49:54 +0100

    refactor service with explicit logging

--- src/main/java/com/framework/galaxion/application/service/SubscriptionService.java
+++ src/main/java/com/framework/galaxion/application/service/SubscriptionService.java
@@ -8,9 +8,11 @@ import com.framework.galaxion.domain.port.in.AddOptionToSubscriptionUseCase;
 import com.framework.galaxion.domain.port.in.GetAllSubscriptionsUseCase;
 import com.framework.galaxion.domain.port.out.SubscriptionRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 public class SubscriptionService implements
         GetAllSubscriptionsUseCase,
@@ -20,17 +22,22 @@ public class SubscriptionService implements
 
     @Override
     public List<Subscription> execute() {
+        log.debug("Fetching all subscriptions");
         return subscriptionRepository.findAll();
     }
 
     @Override
     public SubscriptionOption execute(Long subscriptionId, OptionName optionName) {
+        log.debug("Adding option {} to subscription {}", optionName, subscriptionId);
+
         Subscription subscription = subscriptionRepository.findById(subscriptionId)
                 .orElseThrow(() -> new SubscriptionNotFoundException(subscriptionId));
 
         SubscriptionOption option = subscription.addOption(optionName);
 
         subscriptionRepository.save(subscription);
+
+        log.info("Option {} added to subscription {}", optionName, subscriptionId);
 
         return option;
     }
